43e230dd3dcec088f4a3eeb260d10912cad8a73d
Tom McGee <tmmcgee13@aol.com>
2014-06-23 16:18:42 -0500

    depth of 5

--- RecursiveComputer.rb
+++ RecursiveComputer.rb
@@ -1,92 +1,104 @@
 class RecursiveComputer
 	#1,9,8,3,6
 	def initialize
+    @depth = 0
 
 	end
 
   # rename Turn -> turn
   # prefer to take a tic_tac_toe than a board
   # because the tic_tac_toe can provide abstractions around the things we calculate
-  def Turn(board)
-  	current_turn = false
-  	move =  negamax(board, current_turn, 0, {}, (Float::INFINITY * -1),Float::INFINITY,1).to_i + 1
-  	return move
+   def Turn(board)
+    current_turn = false
+    move =  negamax(board, current_turn, 0, {}, (Float::INFINITY * -1),Float::INFINITY,1,0).to_i + 1
+    return move
   end
 
   private
 
   def num_available_moves(board)
-  	board.size - board.compact.size
+    board.size - board.compact.size
   end
 
-  def negamax(board,turn,depth,best,a,b,child_num)
-  	move_char = (turn ? 'X' : 'O')
-  	if (computer_victory(board) || player_victory(board))
-  		return -10 * (board.size + 1 - depth)
-  	elsif num_available_moves(board) == 0
-  		return 0
-  	end
+  def negamax(board,turn,depth,best,a,b,child_num,start_level)
+    move_char = (turn ? 'X' : 'O')
+    if (computer_victory(board) || player_victory(board))
+      return -10 * (board.size + 1 - depth)
+    elsif num_available_moves(board) == 0
+      return 0
+    end
 
     old_child_num = child_num
     child_num = 0
     beta = b
-  	(0...board.length).each do |i|
+    (0...board.length).each do |i|
 
-  		if (board[i] != nil)
+      if (board[i] != nil)
         # no op
-      else		
-        child_num += 1	
-      	new_board = Array.new(board.length,nil)
-      	(0...board.length).each do |j|
-      		new_board[j] = board[j]
-      	end
-      	new_board[i] = move_char
-      	score = -1 * negamax(new_board, !turn, depth + 1,{},-beta,-a,child_num)
-        best[(i).to_s] = score
-        print "a = " + a.to_s + " b = " + b.to_s
-        if (a < score && score < b && child_num > 1)
-          score = -1 * negamax(new_board, !turn, depth + 1,{},-b,-a,child_num)
-          best[(i).to_s] = score
+      else  
+        if (start_level + 5 < depth )
+          return 0 
+        end
+        child_num += 1  
+        new_board = Array.new(board.length,nil)
+        (0...board.length).each do |j|
+          new_board[j] = board[j]
+        end
+        new_board[i] = move_char
+        score = -1 * negamax(new_board, !turn, depth + 1,{},-beta,-a,child_num,start_level).to_i
+        if (a < score && score < b && old_child_num != 1)
+          score = -1 * negamax(new_board, !turn, depth + 1,{},-b,-a,child_num,start_level).to_i
         end
         a = [score,a].max
-        #best[(i).to_s] = score
-        if a>= b
+        b = a + 1
+        best[(i).to_s] = a
+        if a >= b
           break
-          return a
         end
       end
-
     end
-    b = a + 1
-
+    
     move = best.max_by { |key,value| value }[0]
     high_score = best.max_by { |key, value| value }[1]
     if  depth == 0
-    	return move
+      return move
     else
-    	return a
+      return a
     end
   end
 
   def player_victory(board)
-  	possible_wins(board).each do |possible|
-  		if ((board[possible[0]].eql?(board[possible[1]])) && (board[possible[0]].eql?(board[possible[2]])) && board[possible[0]] != nil && board[possible[0]] == 'X' )
-  			return true
-  		end
-  	end
+  	for possible in possible_wins(board)
+      if board.length == 9
+  		  if ((board[possible[0]].eql?(board[possible[1]])) && (board[possible[0]].eql?(board[possible[2]])) && board[possible[0]] != nil && board[possible[0]] == 'X' )
+  			 return true
+        end
+      else
+        if (board[possible[0]].eql?(board[possible[1]]) && board[possible[0]].eql?(board[possible[2]]) && board[possible[0]].eql?(board[possible[3]]) && board[possible[0]] != nil && board[possible[0]] = 'X')
+  	     return true
+        end
+      end
+    end
   	return false
   end
 
   def computer_victory(board)
-  	possible_wins(board).each do |possible|
-  		if ((board[possible[0]].eql?(board[possible[1]])) && (board[possible[0]].eql?(board[possible[2]])) && board[possible[0]] != nil && board[possible[0]] == 'O' )
-  			return true
-  		end
+  	for possible in possible_wins(board)
+      if board.length == 9
+  	   if ((board[possible[0]].eql?(board[possible[1]])) && (board[possible[0]].eql?(board[possible[2]])) && board[possible[0]] != nil && board[possible[0]] == 'O' )
+         return true
+       end
+      else
+        if (board[possible[0]].eql?(board[possible[1]]) && board[possible[0]].eql?(board[possible[2]]) && board[possible[0]].eql?(board[possible[3]]) && board[possible[0]] != nil && board[possible[0]] == 'O')
+         return true
+        end
+      end
   	end
   	return false
   end
 
   def possible_wins(board)
+  	possible_wins = Array.new
   	if board.length == 9
   		possible_wins = [ [0, 1, 2], [3, 4, 5], [6, 7, 8], [0, 3, 6], [1, 4, 7], [2, 5, 8], [0, 4, 8], [2, 4, 6] ]
   	elsif board.length == 16
