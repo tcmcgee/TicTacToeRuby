a00fdba3b2c26be2665274738d5ccb640393a8bf
Tom McGee <tmmcgee13@aol.com>
2014-06-25 13:25:29 -0500

    removed dependencies

--- Main.rb
+++ Main.rb
@@ -3,16 +3,17 @@ require_relative "Computer"
 require_relative "Game"
 require_relative "RecursiveComputer"
 
+input = $stdin
+output = $stdout
 
+output.print "Please pick your board size!!\n"
 
-puts "Please pick your board size!!\n"
-
-print "1. 3x3\n"
-print "2. 4x4\n"
+output.print "1. 3x3\n"
+output.print "2. 4x4\n"
 valid2 = false
 size = -1
 while (!valid2)
-	choice = gets.chomp.to_i
+	choice = input.gets.chomp.to_i
 	if choice == 1
 		size = 9
 		valid2 = true
@@ -20,21 +21,21 @@ while (!valid2)
   	size = 16
 		valid2 = true
 	else 
-		print "Please give valid input! (1 or 2)"
+		output.print "Please give valid input! (1 or 2)"
 		
 	end
 end
 
 
 
-puts "Please pick your computers difficulty!\n"
+output.print "Please pick your computers difficulty!\n"
 
-print "1. Easy\n"
-print "2. Hard\n"
+output.print "1. Easy\n"
+output.print "2. Hard\n"
 valid = false
 
 while (!valid)
-	choice = gets.chomp.to_i
+	choice = input.gets.chomp.to_i
 	if choice == 1
 		valid = true
 		computer = Computer.new
@@ -42,11 +43,11 @@ while (!valid)
 		valid = true
 		computer = RecursiveComputer.new
 	else
-		print "Please enter a valid input! (1 or 2)\n"
+		output.print "Please enter a valid input! (1 or 2)\n"
 	end
 end
 
 tictactoe = TicTacToe.new(size)
-game = Game.new(tictactoe,computer,$stdin,$stdout)
+game = Game.new(tictactoe,computer,input,output)
 
 game.start
