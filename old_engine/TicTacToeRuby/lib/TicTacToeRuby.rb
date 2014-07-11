b4b94d9c61b1a8467bf3955ca115cacec5b06289
Taka Goto <c_tgoto@groupon.com>
2014-07-11 13:59:09 -0500

    taking out ui

--- old_engine/TicTacToeRuby/lib/TicTacToeRuby.rb
+++
@@ -1,9 +0,0 @@
-
-
-module TicTacToeRuby
-require "TicTacToeRuby/version"
-require "TicTacToeRuby/Recursive_computer"
-require "TicTacToeRuby/Computer"
-require "TicTacToeRuby/Game"
-require "TicTacToeRuby/TicTacToe"
-end
