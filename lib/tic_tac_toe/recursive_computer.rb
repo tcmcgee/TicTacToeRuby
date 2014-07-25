9ff48a03d60f8fe1923f51aaad19875aaf43464e
Taka Goto <c_tgoto@groupon.com>
2014-07-25 14:40:33 -0500

    cleaning up duplicate code

--- lib/tic_tac_toe/recursive_computer.rb
+++ lib/tic_tac_toe/recursive_computer.rb
@@ -1,3 +1,5 @@
+require 'tic_tac_toe/board'
+
 module TicTacToe
   class Recursive_computer
     #1,9,8,3,6
@@ -6,7 +8,6 @@ module TicTacToe
     end
 
     def turn(board)
-      @@possible = possible_wins(board.length)
       if (num_available_moves(board) > 11)
         return random_move(board)
       end
@@ -27,22 +28,24 @@ module TicTacToe
       board.size - board.compact.size
     end
 
-    def negamax(board,turn,depth,best)
+    def negamax(tiles,turn,depth,best)
       move_char = (turn ? 'X' : 'O')
-      if (get_winner(board,turn,@@possible) != nil)
+      board = TicTacToe::Board.new(tiles.length)
+      board.set_tiles(tiles)
+      if (board.get_winner != nil)
         return -10
-      elsif num_available_moves(board) == 0
+      elsif num_available_moves(board.tiles) == 0
         return 0
       end
-      (0...board.length).each do |i|
+      (0...board.tiles.length).each do |i|
 
-        if (board[i] != nil)
+        if (board.tiles[i] != nil)
           # no op
         else
 
-          new_board = Array.new(board.length,nil)
-          (0...board.length).each do |j|
-            new_board[j] = board[j]
+          new_board = Array.new(board.tiles.length,nil)
+          (0...board.tiles.length).each do |j|
+            new_board[j] = board.tiles[j]
           end
           new_board[i] = move_char
           best[(i).to_s] = -1 * negamax(new_board, !turn, depth + 1,{}).to_i
@@ -57,75 +60,5 @@ module TicTacToe
       end
     end
 
-    def get_winner(board,turn,wins = possible_wins(board))
-      per_row = Math.sqrt(board.length)
-      wins.each do |possible|
-        counter = 0
-        (0...per_row).each do |index|
-          if (board[possible[index]] == board[possible[0]] && board[possible[0]] != nil)
-            counter = counter + 1
-          end
-        end
-        if (counter == per_row)
-          return (turn ? 'X' : 'O')
-        end
-      end
-      return nil
-    end
-
-     def get_horizontal_wins(board_length)
-    per_row = Math.sqrt(board_length)
-    possible = []
-    (0...board_length).each do |i|
-      possible.push(i)
-    end
-
-    possible = possible.each_slice(per_row).to_a
   end
-
-  def get_vertical_wins(horizontal,board_length)
-    vertical = []
-    possible = []
-    per_row = Math.sqrt(board_length)
-    (0...per_row).each do |row|
-      vertical = []
-      (0...per_row).each do |index|
-        row_array = horizontal[index]
-        vertical.push(row_array[row])
-      end
-      possible.push(vertical)
-    end
-    return possible
-  end
-
-  def get_diagonal_wins(possible,board_length)
-    per_row = Math.sqrt(board_length)
-    diag = []
-    if ((board_length % 2) == 1)
-      diag = []
-      row = 0
-      (0...per_row).each do |column|
-        row_array = possible[column]
-        diag.push(row_array[row])
-        row = row + 1
-      end
-      possible.push(diag)
-      diag = []
-      row = per_row - 1
-      (0...per_row).each do |column|
-        row_array = possible[column]
-        diag.push(row_array[row])
-        row = row - 1
-      end
-      possible.push(diag)
-    end
-  end
-
-  def possible_wins(board_length)
-    possible = get_horizontal_wins(board_length)
-    possible = possible + get_vertical_wins(possible,board_length)
-    possible = possible + get_diagonal_wins(possible,board_length)
-    return possible
-  end
-end
 end
--- spec/recursive_computer_spec.rb
+++ spec/recursive_computer_spec.rb
@@ -31,14 +31,6 @@ describe Recursive_computer do
     expect(computer.turn(board)).to eq(2)
   end
 
-  it "returns possible wins for a 3x3" do
-    computer.possible_wins(9) =~ [
-      [0, 1, 2], [0, 3, 6], [1, 4, 7],
-      [3, 4, 5], [2, 5, 8], [0, 4, 8],
-      [6, 7, 8], [2, 4, 6]
-    ]
-  end
-
   it "Prefers winning over blocking a player win" do
     board = [nil,nil,nil,
              'O','O',nil,
@@ -73,30 +65,6 @@ describe Recursive_computer do
              nil,'O',nil,
              'X','O',nil]
     expect(computer.num_available_moves(board)).to eq(3)
-  end
-  it "can get the winner of the game" do
-        board = ['O','X','X',
-                'O','O','X',
-                'X','O','X']
-        expect(computer.get_winner(board,true,computer.possible_wins(board.length))).to eq('X')
-  end
-  it "returns nil if the game has no winner" do
-        board = ['O','X','O',
-                'O','O','X',
-                'X','O','X']
-        expect(computer.get_winner(board,true,computer.possible_wins(board.length))).to eq(nil)
-  end
-
-    it "can get the horizontal victory for any sized board" do
-    computer.get_horizontal_wins(9) =~ [[0,1,2],[3,4,5],[6,7,8]] 
-  end
-
-  it "can get the vertical victory for any sized board" do
-    computer.get_vertical_wins([[0,1,2],[3,4,5],[6,7,8]],9) =~ [[0,3,6],[1,4,7],[2,5,8]] 
-  end
-
-  it "can get the diagonal victory for any sized board" do
-    computer.get_diagonal_wins([[0,1,2],[3,4,5],[6,7,8],[0,3,6],[1,4,7],[2,5,8]],9) =~ [[0,4,8],[2,4,6]] 
   end
 
   xit "Never loses a game when the player goes first", t:true, :speed => 'slow'do
