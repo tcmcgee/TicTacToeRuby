5bde1982b7656b4d1ff11e5942a75756011111e9
Tom McGee <tmmcgee13@aol.com>
2015-06-22 14:31:42 -0500

    more path errors

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -1,4 +1,4 @@
-require '../lib/tic_tac_toe/game'
-require '../lib/tic_tac_toe/computer'
-require '../lib/tic_tac_toe/recursive_computer'
-require '../lib/tic_tac_toe/board'
+require './lib/tic_tac_toe/game'
+require './lib/tic_tac_toe/computer'
+require './lib/tic_tac_toe/recursive_computer'
+require './lib/tic_tac_toe/board'
